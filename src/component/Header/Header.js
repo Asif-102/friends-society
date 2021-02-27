469d832e33a479e7373e932023ca351b94a90cc7
Asif2535 <aiqbal517@gmail.com>
2021-02-27 19:23:25 +0600

    create component

---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../images/header-logo.png';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img src={logo} alt=""/>
+        </div>
+    );
+};
+
+export default Header;<h1>This is Heading</h1>
---
+++ src/component/suggestFriend/Friend.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Friend.css'
+
+const Friend = () => {
+    return (
+        <div className="suggestFriend">
+            <h2>Suggest Section</h2>
+        </div>
+    );
+};
+
+export default Friend;
